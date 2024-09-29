64b85dd4d7bbe804540e84870da45dbf8bdf19b0
itsumi <122093200+itsumiq@users.noreply.github.com>
2024-09-29 14:18:21 +0300

    feat: add metadata

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,5 +1,11 @@
 import { Contacts } from '@/src/views/Contacts/ui';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Contacts | Dictionary',
+};
+
 const Connection = () => {
   return <Contacts />;
 };
--- app/dictionary/[word]/page.tsx
+++ app/dictionary/[word]/page.tsx
@@ -2,9 +2,17 @@
 
 import { Word } from '@/src/views/Word';
 
+import type { Metadata } from 'next';
+
 interface DictionaryProps {
   params: { word: string };
 }
+
+export const generateMetadata = async ({ params }: DictionaryProps): Promise<Metadata> => {
+  return {
+    title: `${params.word} | Dictionary`,
+  };
+};
 
 const Dictionary = async ({ params }: DictionaryProps) => {
   const searchWord = params.word;
